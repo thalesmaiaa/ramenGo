6b6706ef294c90c8d8531d50effafa86faec48f4
Thales Maia <thalesmaiaalexandre@gmail.com>
2024-05-29 16:48:30 -0300

    build: First commit and configuration of repository files

---
+++ src/main/java/com/ramengo/RamengoApplication.java
@@ -0,0 +1,13 @@
+package com.ramengo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RamengoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RamengoApplication.class, args);
+	}
+
+}
