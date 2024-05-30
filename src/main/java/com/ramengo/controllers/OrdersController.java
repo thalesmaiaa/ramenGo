d16d09ab18b5d016031e9ba02fb03ec27b8e2a39
Thales Maia <thalesmaiaalexandre@gmail.com>
2024-05-30 16:30:02 -0300

    chg: changing routes path

--- src/main/java/com/ramengo/controllers/OrdersController.java
+++ src/main/java/com/ramengo/controllers/OrdersController.java
@@ -10,9 +10,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
-@RequestMapping("/v1/orders")
+@RequestMapping("/orders")
 public class OrdersController extends AbstractController {
 
     @Value("${x-api-key}")
